6536171780dbaaef59cca5c59c9324eb6f4c4076
Multi-Project-Virify <fEAxLG5NYrK6bR80@proton.me>
2026-07-04 18:29:01 +0500

    Create __init__.py

---
+++ core/__init__.py
@@ -0,0 +1,5 @@
+from core.context import TranslationContext
+from core.engine import TranslationEngine
+from core.interfaces import Module
+
+__all__ = ["TranslationContext", "TranslationEngine", "Module"]
