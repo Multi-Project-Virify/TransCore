8cdc67781cbfd9692e09c7dacdbc59fe8a8d50bb
Multi-Project-Virify <fEAxLG5NYrK6bR80@proton.me>
2026-07-04 18:20:46 +0500

    Update __init__.py

--- core/init.py
+++ core/init.py
@@ -1,1 +1,5 @@
-#Пустой
+from core.context import TranslationContext
+from core.engine import TranslationEngine
+from core.interfaces import Module
+
+__all__ = ["TranslationContext", "TranslationEngine", "Module"]
