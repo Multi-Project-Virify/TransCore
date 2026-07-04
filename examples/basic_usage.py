1f3841ba7243c2898f467dcdf6cc2fdc4bfcd698
Multi-Project-Virify <fEAxLG5NYrK6bR80@proton.me>
2026-07-04 18:43:14 +0500

    Update basic_usage.py

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,1 +1,19 @@
-#Пустой
+from core.engine import TranslationEngine
+from modules.tokenizer import Tokenizer
+from modules.detector import LanguageDetector
+from modules.dictionary import Dictionary
+from modules.ranking import Ranking
+
+engine = TranslationEngine()
+engine.add_module(Tokenizer())
+engine.add_module(LanguageDetector())
+engine.add_module(Dictionary())
+engine.add_module(Ranking())
+
+result = engine.translate("I went to the river bank")
+print(result.result)
+# Ожидаем: ... берег (не банк, т.к. "river" рядом)
+
+result2 = engine.translate("I opened a bank account")
+print(result2.result)
+# Ожидаем: ... банк (не берег, т.к. "account" рядом)
