316397a44b716b798d89d0c74d084e14f6d203ac
Multi-Project-Virify <fEAxLG5NYrK6bR80@proton.me>
2026-07-04 18:45:55 +0500

    Update test_tokenizer.py

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,1 +1,8 @@
-#Пустой
+from modules.tokenizer import Tokenizer
+from core.context import TranslationContext
+
+
+def test_tokenizer_basic():
+    ctx = TranslationContext(text="Hello, world!")
+    Tokenizer().process(ctx)
+    assert ctx.tokens == ["Hello", ",", "world", "!"]
